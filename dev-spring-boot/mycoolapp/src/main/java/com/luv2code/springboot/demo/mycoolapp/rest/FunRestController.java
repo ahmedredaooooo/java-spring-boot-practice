656936342cb27f08c00424d1a6013b19177d8115
ahmedredaooooo <ahmedredaqadry@gmail.com>
2025-06-09 16:36:03 +0300

    Add coach and team properties, configure port 8484, and inject values into FunRestController

--- dev-spring-boot/mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ dev-spring-boot/mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,11 @@ public class FunRestController {
     public String sayHello() {
         return "Hello World!";
     }
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
 }
