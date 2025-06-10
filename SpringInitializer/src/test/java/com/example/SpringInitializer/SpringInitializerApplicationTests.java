fbb6b4d81c82e18149fafc0feab815ea215a54c0
ahmedredaooooo <ahmedredaqadry@gmail.com>
2025-06-10 19:40:00 +0300

    Initialize new Spring Boot project with basic setup using Spring Initializr and verified application startup

---
+++ SpringInitializer/src/test/java/com/example/SpringInitializer/SpringInitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringInitializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
