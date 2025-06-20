59f8ee281ffdc3de681d623fce405aef01777e37
ahmedredaooooo <ahmedredaqadry@gmail.com>
2025-06-20 09:09:14 +0300

    printing messages

---
+++ 01-java-fundamentals/demo/src/main/java/Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World of Java!");
+        System.out.println("My favorite color is blue.");
+        System.out.println("My hobby is coding.");
+    }
+}
