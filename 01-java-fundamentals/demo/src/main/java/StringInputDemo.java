51fa2b2a30164c3f394d08d80d03d8937e3bfc21
ahmedredaooooo <ahmedredaqadry@gmail.com>
2025-06-20 09:27:21 +0300

    making color, hobby dynamic only in print statments

---
+++ 01-java-fundamentals/demo/src/main/java/StringInputDemo.java
@@ -0,0 +1,12 @@
+public class StringInputDemo {
+
+    public static void main(String[] args) {
+
+        String theColor = "blue";
+        String theHobby = "coding";
+
+
+        System.out.println("My favourite color is " + theColor);
+        System.out.println("My hobby is " + theHobby);
+    }
+}
