cace566392d76290c63c0cf583ed56068f8326eb
ahmedredaooooo <ahmedredaqadry@gmail.com>
2025-06-20 18:44:26 +0300

    if-else with user prompt

--- java-core/02-conditions/src/main/java/ConditionalDemoBasicIfElse.java
+++ java-core/02-conditions/src/main/java/ConditionalDemoBasicIfElse.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
+
 public class ConditionalDemoBasicIfElse {
     public static void main(String[] args) {
 
-        int minVotingAge = 18, age = 15;
+        Scanner sc = new Scanner(System.in);
+
+        int minVotingAge = 18;
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
         System.out.println("Minimum voting age is " + minVotingAge);
 
-        if (age >= minVotingAge) {
+        boolean eligible = age >= minVotingAge;
+        System.out.println("eligible is " + eligible + '\n');
+
+        if (eligible) {
             System.out.println("You are eligible to vote.");
         } else {
             System.out.println("You are NOT eligible to vote.");
         }
+
+        sc.close();
     }
+
 }
