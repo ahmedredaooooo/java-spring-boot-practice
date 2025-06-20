8e603b94e4db1804b79728f21712e819f56d7f39
ahmedredaooooo <ahmedredaqadry@gmail.com>
2025-06-20 16:05:04 +0300

    organize folders after merging java, spring repos

--- 01-java-fundamentals/demo/src/main/java/Demo.java
+++
@@ -1,7 +0,0 @@
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("Hello World of Java!");
-        System.out.println("My favorite color is blue.");
-        System.out.println("My hobby is coding.");
-    }
-}
