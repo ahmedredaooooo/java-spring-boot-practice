5d496defa6e0a7c4f69c8355c6db0f98ebedb3cf
ahmedredaooooo <ahmedredaqadry@gmail.com>
2025-06-24 12:31:16 +0300

    method with parameters

--- java-core/04-methods/src/main/java/MethodDemo.java
+++ java-core/04-methods/src/main/java/MethodDemo.java
@@ -3,7 +3,20 @@ public class MethodDemo {
     public static void main(String[] args) {
 
         // call the method
-        displayGreetings();
+        // displayGreetings();
+
+        displayManyGreetings(3);
+    }
+
+    static void displayManyGreetings(int count) {
+        for (int i = 1; i <= count; i++) {
+
+            System.out.println("Hello World!");
+            System.out.println("Welcome to Java!");
+            System.out.println("Please make yourself at home.");
+
+            System.out.println();
+        }
     }
 
     // define the method
