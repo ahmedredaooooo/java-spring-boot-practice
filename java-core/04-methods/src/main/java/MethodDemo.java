7223d64cd91b4d017fb34fea89d4f08797aa4f2f
ahmedredaooooo <ahmedredaqadry@gmail.com>
2025-06-24 12:25:15 +0300

    getting started with methods

---
+++ java-core/04-methods/src/main/java/MethodDemo.java
@@ -0,0 +1,15 @@
+public class MethodDemo {
+
+    public static void main(String[] args) {
+
+        // call the method
+        displayGreetings();
+    }
+
+    // define the method
+    static void displayGreetings() {
+        System.out.println("Hello World!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Please make yourself at home.");
+    }
+}
