ce4af1064984ce5944b0b9c02fe2afbd9e181de3
ahmedredaooooo <ahmedredaqadry@gmail.com>
2025-06-25 21:00:06 +0300

    control array based on user input

--- java-core/arrays/src/main/java/StudentGradesDemo.java
+++ java-core/arrays/src/main/java/StudentGradesDemo.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class StudentGradesDemo {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("hard coded");
         // initialize the array
         double[] grades = new double[3];
 
@@ -13,5 +17,24 @@ public class StudentGradesDemo {
         // display the grades
         for (double grade : grades)
             System.out.println(grade);
+        System.out.println();
+
+        System.out.println("user input based:- ");
+
+        System.out.print("How many grades will you entre? ");
+        int numOfGrades = sc.nextInt();
+
+        grades = new double[numOfGrades];
+        for (int i = 0; i < numOfGrades; i++) {
+            System.out.print("Enter grade #" + (i + 1) + ": ");
+            grades[i] = sc.nextDouble();
+        }
+
+        System.out.println("your grades are:-");
+        // display the grades
+        for (double grade : grades)
+            System.out.println(grade);
+
+        sc.close();
     }
 }
