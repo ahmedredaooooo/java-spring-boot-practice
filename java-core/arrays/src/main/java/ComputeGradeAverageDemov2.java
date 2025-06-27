a08b6d54112f7d28d1fb899d2430a9bed963b89c
ahmedredaooooo <ahmedredaqadry@gmail.com>
2025-06-27 23:59:46 +0300

    return array from method

---
+++ java-core/arrays/src/main/java/ComputeGradeAverageDemov2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ComputeGradeAverageDemov2 {
+
+    public static void main(String[] args) {
+
+        double[] grades = getDoubles();
+
+
+        System.out.println("your grades are:-");
+        // display the grades
+        for (double grade : grades)
+            System.out.println(grade);
+
+        double gradeAverage = computeGradeAverage(grades);
+        System.out.println("Your grade average is: " + gradeAverage);
+
+    }
+
+    private static double[] getUserGrades() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many grades will you entre? ");
+        int numOfGrades = sc.nextInt();
+
+        double[] grades = new double[numOfGrades];
+        for (int i = 0; i < numOfGrades; i++) {
+            System.out.print("Enter grade #" + (i + 1) + ": ");
+            grades[i] = sc.nextDouble();
+        }
+        sc.close();
+        return grades;
+    }
+
+    private static double computeGradeAverage(double[] grades) {
+
+        // compute sum of the grades
+       double sum = 0;
+       for (double grade : grades) {
+           sum += grade;
+       }
+
+       // compute grade average, based on length of array
+        return sum / grades.length;
+    }
+}
