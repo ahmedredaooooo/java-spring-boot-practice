8dadf849231305a1f5208d2f697946549348a1e8
ahmedredaooooo <ahmedredaqadry@gmail.com>
2025-07-04 10:44:56 +0300

    Add multiple Enemy instances t demonstrate object creation and method usage

--- java-core/06-Java-OOP/src/main/java/battlearena/Main.java
+++ java-core/06-Java-OOP/src/main/java/battlearena/Main.java
@@ -5,12 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Enemy zombie = new Enemy("Zombie", 10, 1);
 
-        System.out.println(zombie + "\n");
+        Enemy ogre = new Enemy("Ogre", 20, 3);
 
         zombie.talk();
 
-        zombie.walkForward();
+        ogre.talk();
 
-        zombie.attack();
     }
 }
